887010d3e9b153fddc636e04a897eea78397a0f3
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-09-21 23:07:06 +0530

    Add about app with college info view and update URL patterns

--- 01_First_Request_Response/My_first_project/My_first_project/settings.py
+++ 01_First_Request_Response/My_first_project/My_first_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 EXTERNAL_APPS = [
     'home',
+    'about',
 ]
 
 INSTALLED_APPS += EXTERNAL_APPS
--- 01_First_Request_Response/My_first_project/My_first_project/urls.py
+++ 01_First_Request_Response/My_first_project/My_first_project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+from about.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", welcome, name='welcome'),
+    path("about/", collegeinfo, name='about'),
 
 ]
