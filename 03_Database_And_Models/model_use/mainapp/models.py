527cdca40b93804724202e0c9abdac591ccdd8f6
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-09-26 22:00:52 +0530

    Add student model with fields for name, age, email, address, and roll number; create initial migration files

--- 03_Database_And_Models/model_use/mainapp/models.py
+++ 03_Database_And_Models/model_use/mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class student(models.Model):
+
+    # id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    rollno = models.IntegerField()
+    age = models.IntegerField(default=18)
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True)
+
+
+    
